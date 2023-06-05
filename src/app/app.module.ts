993a58c408c0b302187f962c9c34cd82b0182a64
tallyto <rodrigues.tallyto@hotmail.com>
2023-06-05 11:53:43 -0300

    11.8 desafio usando pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {LOCALE_ID, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {TabViewModule} from "primeng/tabview";
 import {InputTextModule} from "primeng/inputtext";
 import {ButtonModule} from "primeng/button";
 import {TableModule} from "primeng/table";
 import {TooltipModule} from "primeng/tooltip";
-import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import {LancamentosPesquisaComponent} from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {PessoasPesquisaComponent} from './pessoas-pesquisa/pessoas-pesquisa.component';
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -19,16 +21,19 @@ import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.co
     NavbarComponent,
     PessoasPesquisaComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        TabViewModule,
-        InputTextModule,
-        ButtonModule,
-        TableModule,
-        TooltipModule,
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TabViewModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule,
+    FormsModule,
+    CommonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-lancamentos-pesquisa',
@@ -7,26 +7,104 @@ import { Component } from '@angular/core';
 })
 export class LancamentosPesquisaComponent {
 
-  lancamentos : Lancamento [] = [
-    {tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017', dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José'},
-    {tipo: 'RECEITA', descricao: 'Venda de produtos', dataVencimento: null, dataPagamento: '15/05/2023', valor: 500.00, pessoa: 'Cliente A'},
-    {tipo: 'DESPESA', descricao: 'Pagamento do aluguel', dataVencimento: '10/05/2023', dataPagamento: '12/05/2023', valor: 1000.00, pessoa: 'João da Silva'},
-    {tipo: 'DESPESA', descricao: 'Compra de material de escritório', dataVencimento: '25/06/2023', dataPagamento: '30/06/2023', valor: 50.75, pessoa: 'Papelaria ABC'},
-    {tipo: 'RECEITA', descricao: 'Reembolso de despesas', dataVencimento: null, dataPagamento: '20/06/2023', valor: 250.00, pessoa: 'Empresa XYZ'},
-    {tipo: 'DESPESA', descricao: 'Pagamento de telefone', dataVencimento: '15/06/2023', dataPagamento: '20/06/2023', valor: 75.50, pessoa: 'Operadora XYZ'},
-    {tipo: 'DESPESA', descricao: 'Compra de comida', dataVencimento: '05/06/2023', dataPagamento: '07/06/2023', valor: 30.00, pessoa: 'Mercado Superbom'},
-    {tipo: 'RECEITA', descricao: 'Recebimento de honorários', dataVencimento: null, dataPagamento: '25/06/2023', valor: 1000.00, pessoa: 'Cliente B'},
-    {tipo: 'DESPESA', descricao: 'Pagamento de internet', dataVencimento: '20/06/2023', dataPagamento: '22/06/2023', valor: 85.90, pessoa: 'Provedor de Internet'},
-    {tipo: 'DESPESA', descricao: 'Compra de roupas', dataVencimento: '18/06/2023', dataPagamento: '25/06/2023', valor: 200.00, pessoa: 'Loja Fashion'},
-    {tipo: 'RECEITA', descricao: 'Venda de serviços', dataVencimento: null, dataPagamento: '28/06/2023', valor: 800.00, pessoa: 'Cliente C'}
-  ]
+  public lancamentos: Lancamento[] = [
+    {
+      tipo: 'DESPESA',
+      descricao: 'Compra de pão',
+      dataVencimento: new Date('2023-06-30'),
+      dataPagamento: new Date('2023-06-25'),
+      valor: 4.55,
+      pessoa: 'Padaria do José'
+    },
+    {
+      tipo: 'RECEITA',
+      descricao: 'Venda de produtos',
+      dataVencimento: new Date('2023-06-25'),
+      dataPagamento: new Date('2023-05-15'),
+      valor: 500.00,
+      pessoa: 'Cliente A'
+    },
+    {
+      tipo: 'DESPESA',
+      descricao: 'Pagamento do aluguel',
+      dataVencimento: new Date('2023-05-10'),
+      dataPagamento: new Date('2023-05-12'),
+      valor: 1000.00,
+      pessoa: 'João da Silva'
+    },
+    {
+      tipo: 'DESPESA',
+      descricao: 'Compra de material de escritório',
+      dataVencimento: new Date('2023-06-25'),
+      dataPagamento: new Date('2023-06-30'),
+      valor: 50.75,
+      pessoa: 'Papelaria ABC'
+    },
+    {
+      tipo: 'RECEITA',
+      descricao: 'Reembolso de despesas',
+      dataVencimento: new Date('2023-06-25'),
+      dataPagamento: new Date('2023-06-20'),
+      valor: 250.00,
+      pessoa: 'Empresa XYZ'
+    },
+    {
+      tipo: 'DESPESA',
+      descricao: 'Pagamento de telefone',
+      dataVencimento: new Date('2023-06-15'),
+      dataPagamento: new Date('2023-06-20'),
+      valor: 75.50,
+      pessoa: 'Operadora XYZ'
+    },
+    {
+      tipo: 'DESPESA',
+      descricao: 'Compra de comida',
+      dataVencimento: new Date('2023-06-05'),
+      dataPagamento: new Date('2023-06-07'),
+      valor: 30.00,
+      pessoa: 'Mercado Superbom'
+    },
+    {
+      tipo: 'RECEITA',
+      descricao: 'Recebimento de honorários',
+      dataVencimento: new Date('2023-06-25'),
+      dataPagamento: new Date('2023-06-25'),
+      valor: 1000.00,
+      pessoa: 'Cliente B'
+    },
+    {
+      tipo: 'DESPESA',
+      descricao: 'Pagamento de internet',
+      dataVencimento: new Date('2023-06-20'),
+      dataPagamento: new Date('2023-06-22'),
+      valor: 85.90,
+      pessoa: 'Provedor de Internet'
+    },
+    {
+      tipo: 'DESPESA',
+      descricao: 'Compra de roupas',
+      dataVencimento: new Date('2023-06-18'),
+      dataPagamento: new Date('2023-06-25'),
+      valor: 200.00,
+      pessoa: 'Loja Fashion'
+    },
+    {
+      tipo: 'RECEITA',
+      descricao: 'Venda de serviços',
+      dataVencimento: new Date('2023-06-25'),
+      dataPagamento: new Date('2023-06-28'),
+      valor: 800.00,
+      pessoa: 'Cliente C'
+    }
+  ];
 
 }
+
 interface Lancamento {
   tipo: string;
   descricao: string;
-  dataVencimento: string | null;
-  dataPagamento: string | null;
+  dataVencimento: Date;
+  dataPagamento: Date;
   valor: number;
   pessoa: string;
 }
