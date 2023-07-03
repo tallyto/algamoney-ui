4ded3b39ba5957509141d6fdbac08ae3d7779a0a
tallyto <rodrigues.tallyto@hotmail.com>
2023-07-03 19:34:08 -0300

    feat: add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { PessoasCadastroComponent } from './pessoas-cadastro/pessoas-cadastro.component';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PessoasPesquisaComponent},
+  {path: 'pessoa-pesquisa', component: PessoasPesquisaComponent},
+  {path: 'pessoa-cadastro', component: PessoasCadastroComponent},
+  {path: 'lancamento-pesquisa', component: LancamentosPesquisaComponent},
+  {path: 'lancamento-cadastro', component: LancamentoCadastroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
