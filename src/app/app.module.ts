a0b13aaf76e766934808cc9ee824734b3d46380c
tallyto <rodrigues.tallyto@hotmail.com>
2023-07-08 19:20:50 -0300

    14.11 - desafio criando o core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,22 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {NavbarComponent} from './navbar/navbar.component';
 import {LancamentosModule} from "./lancamentos/lancamentos.module";
 import {PessoasModule} from "./pessoas/pessoas.module";
 import {SharedModule} from "./shared/shared.module";
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
     LancamentosModule,
-    PessoasModule
+    PessoasModule,
+    CoreModule
   ],
   providers: [],
   exports: [
