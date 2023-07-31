e3b8c94989700a452bf9da41ce47c4472d5a0ae3
tallyto <rodrigues.tallyto@hotmail.com>
2023-07-31 11:20:58 -0300

    23.04 - buscando dados do grafico de pizza

---
+++ src/app/dashboard/dashboard.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+  baseUrl: string
+
+
+  constructor(private http: HttpClient) {
+    this.baseUrl = `${environment.apiUrl}`
+  }
+
+  public lancamentosPorCategoria(){
+     return this.http.get(`${this.baseUrl}/lancamentos/estatisticas/por-categoria`);
+  }
+}
--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,30 +1,69 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DashboardService} from "../dashboard.service";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   pieChartData: any;
   lineChartData: any;
 
-  constructor() {
-    this.generatePieChartData();
+  constructor(private dashboardService: DashboardService) {
     this.generateLineChartData();
   }
 
-  generatePieChartData() {
-    this.pieChartData = {
-      labels: ['Despesas', 'Receitas'],
-      datasets: [
-        {
-          data: [this.getRandomNumber(1000, 5000), this.getRandomNumber(2000, 8000)],
-          backgroundColor: ['red', 'blue'],
-        },
-      ],
-    };
+  ngOnInit() {
+    this.configurarGraficoPizza()
   }
+
+  configurarGraficoPizza() {
+    this.dashboardService.lancamentosPorCategoria().subscribe({
+      next: (value: any) => {
+        this.pieChartData = {
+          labels: value.map((result: LancamentoCategoria) => result.categoria.nome),
+          datasets: [
+            {
+              data: value.map((dado: LancamentoCategoria) => dado.total),
+              backgroundColor: this.generateBeautifulRandomColors(value.length),
+            },
+          ],
+        };
+      }
+    })
+  }
+
+  generateRandomColors(numColors: number): string[] {
+    const colors: string[] = [];
+    const letters = '0123456789ABCDEF';
+
+    for (let i = 0; i < numColors; i++) {
+      let color = '#';
+      for (let j = 0; j < 6; j++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      colors.push(color);
+    }
+
+    return colors;
+  }
+
+   generateBeautifulRandomColors(numColors: number): string[] {
+    const colors: string[] = [];
+
+    for (let i = 0; i < numColors; i++) {
+      const hue = Math.floor(Math.random() * 360); // Valor de 0 a 359 para representar o matiz (cor).
+      const saturation = 70 + Math.random() * 30; // Valores de saturação entre 70 e 100.
+      const lightness = 50 + Math.random() * 10; // Valores de luminosidade entre 50 e 60.
+
+      const color = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+      colors.push(color);
+    }
+
+    return colors;
+  }
+
 
   generateLineChartData() {
     this.lineChartData = {
@@ -57,4 +96,12 @@ export class DashboardComponent {
   getRandomNumber(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
+}
+
+interface LancamentoCategoria {
+  categoria: {
+    codigo: number,
+    nome: string
+  },
+  total: number
 }
